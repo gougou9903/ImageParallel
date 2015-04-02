66d95a7b21550516628289dbca7d62895ae11453
Haoran Fang <gougou9903@gmail.com>
2015-04-01 18:57:16 -0600

    can select an area now

--- UI.java
+++ UI.java
@@ -22,129 +22,129 @@ import javax.swing.JTextField;
 
 
 public class UI extends JFrame implements ActionListener, ItemListener {
-
-	private JFrame frame;
-	File[] sf;
-	JButton btnES = new JButton("Enlarge-Smoothing");
-	JButton btnChooseFile = new JButton("Choose File");
-	private final JLabel lblChosenFile = new JLabel("Chosen File:");
-	private final JLabel lblChosenFileContent = new JLabel("");
-	private final JLabel lblProcessingMethod = new JLabel("Processing Method: ");
-	private final JComboBox comboBoxPM = new JComboBox();
-	private final JLabel lblEnlargeRatio = new JLabel("Enlarge Ratio: ");
-	private JTextField textFieldER;
-
-	String method = "Smoothing";
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UI window = new UI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public UI() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 630, 461);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		
-		btnES.addActionListener(this);
-		btnES.setBounds(458, 404, 166, 29);
-		frame.getContentPane().add(btnES);
-		btnChooseFile.setForeground(Color.BLACK);
-		btnChooseFile.setBackground(Color.RED);
-		
-		btnChooseFile.addActionListener(this);
-		btnChooseFile.setBounds(6, 404, 117, 29);
-		frame.getContentPane().add(btnChooseFile);
-		lblChosenFile.setBounds(6, 187, 85, 50);
-		
-		frame.getContentPane().add(lblChosenFile);
-		lblChosenFileContent.setOpaque(true);
-		lblChosenFileContent.setVerticalAlignment(SwingConstants.TOP);
-		lblChosenFileContent.setBackground(Color.ORANGE);
-		lblChosenFileContent.setBounds(107, 187, 504, 100);
-		
-		frame.getContentPane().add(lblChosenFileContent);
-		lblProcessingMethod.setBounds(6, 27, 127, 50);
-		
-		frame.getContentPane().add(lblProcessingMethod);
-		comboBoxPM.setBounds(139, 27, 133, 50);
-		
-		frame.getContentPane().add(comboBoxPM);
-		comboBoxPM.addItem("Smoothing");
-		comboBoxPM.addItemListener(this);
-		lblEnlargeRatio.setBounds(309, 27, 90, 50);
-		
-		frame.getContentPane().add(lblEnlargeRatio);
-		
-		textFieldER = new JTextField();
-		textFieldER.setBounds(400, 38, 68, 28);
-		frame.getContentPane().add(textFieldER);
-		textFieldER.setColumns(10);
-		textFieldER.setText("1");
-		textFieldER.addActionListener(this);
-	}
-	
-	public void actionPerformed(ActionEvent e)
-	{
-		if(e.getSource() == btnChooseFile){
-			JFileChooser chooser = new JFileChooser();
-    		chooser.setMultiSelectionEnabled(true);
-    		int option = chooser.showOpenDialog(this);
-    		sf = chooser.getSelectedFiles();
-    		String filelist = "nothing";
+    
+    private JFrame frame;
+    File[] sf;
+    JButton btnES = new JButton("Enlarge-Smoothing");
+    JButton btnChooseFile = new JButton("Choose File");
+    private final JLabel lblChosenFile = new JLabel("Chosen File:");
+    private final JLabel lblChosenFileContent = new JLabel("");
+    private final JLabel lblProcessingMethod = new JLabel("Processing Method: ");
+    private final JComboBox comboBoxPM = new JComboBox();
+    private final JLabel lblEnlargeRatio = new JLabel("Enlarge Ratio: ");
+    private JTextField textFieldER;
+    
+    String method = "Smoothing";
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UI window = new UI();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    /**
+     * Create the application.
+     */
+    public UI() {
+        initialize();
+    }
+    
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 630, 461);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        
+        btnES.addActionListener(this);
+        btnES.setBounds(458, 404, 166, 29);
+        frame.getContentPane().add(btnES);
+        btnChooseFile.setForeground(Color.BLACK);
+        btnChooseFile.setBackground(Color.RED);
+        
+        btnChooseFile.addActionListener(this);
+        btnChooseFile.setBounds(6, 404, 117, 29);
+        frame.getContentPane().add(btnChooseFile);
+        lblChosenFile.setBounds(6, 187, 85, 50);
+        
+        frame.getContentPane().add(lblChosenFile);
+        lblChosenFileContent.setOpaque(true);
+        lblChosenFileContent.setVerticalAlignment(SwingConstants.TOP);
+        lblChosenFileContent.setBackground(Color.ORANGE);
+        lblChosenFileContent.setBounds(107, 187, 504, 100);
+        
+        frame.getContentPane().add(lblChosenFileContent);
+        lblProcessingMethod.setBounds(6, 27, 127, 50);
+        
+        frame.getContentPane().add(lblProcessingMethod);
+        comboBoxPM.setBounds(139, 27, 133, 50);
+        
+        frame.getContentPane().add(comboBoxPM);
+        comboBoxPM.addItem("Smoothing");
+        comboBoxPM.addItemListener(this);
+        lblEnlargeRatio.setBounds(309, 27, 90, 50);
+        
+        frame.getContentPane().add(lblEnlargeRatio);
+        
+        textFieldER = new JTextField();
+        textFieldER.setBounds(400, 38, 68, 28);
+        frame.getContentPane().add(textFieldER);
+        textFieldER.setColumns(10);
+        textFieldER.setText("1");
+        textFieldER.addActionListener(this);
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == btnChooseFile){
+            JFileChooser chooser = new JFileChooser();
+            chooser.setMultiSelectionEnabled(true);
+            int option = chooser.showOpenDialog(this);
+            sf = chooser.getSelectedFiles();
+            String filelist = "nothing";
             if (sf.length > 0) filelist = "<html>" + sf[0].getName();
             for(int i = 1; i< sf.length; i++){
-            	if((i+1)%6 == 0){
-            		filelist += "<br>" + sf[i].getName();
-            	}else{
-            	filelist += ", " + sf[i].getName();
-            	}
+                if((i+1)%6 == 0){
+                    filelist += "<br>" + sf[i].getName();
+                }else{
+                    filelist += ", " + sf[i].getName();
+                }
             }
             lblChosenFileContent.setText(filelist);
-		}else if(e.getSource() == btnES){
-			if(sf.length == 1){
-			smoothing smoothing = new smoothing(sf[0].getPath());
-			System.out.println(sf[0].getPath());
-			smoothing.setVisible(true);
-			}else
-				JOptionPane.showMessageDialog(null, "please use one image");
-		}
-	}
-	
-	public void itemStateChanged(ItemEvent i)
+        }else if(e.getSource() == btnES){
+            if(sf.length == 1){
+                smoothing smoothing = new smoothing(sf[0].getPath());
+                System.out.println(sf[0].getPath());
+                smoothing.setVisible(true);
+            }else
+                JOptionPane.showMessageDialog(null, "please use one image");
+        }
+    }
+    
+    public void itemStateChanged(ItemEvent i)
     {
-		if(i.getSource() == comboBoxPM){
-			int state = i.getStateChange();
-			if(state == ItemEvent.SELECTED){
-				ItemSelectable is = i.getItemSelectable();
-		    	Object select[] = is.getSelectedObjects();
-		    	this.method = (String)select[0];
-		    	System.out.println(method);
-			}
-		}
+        if(i.getSource() == comboBoxPM){
+            int state = i.getStateChange();
+            if(state == ItemEvent.SELECTED){
+                ItemSelectable is = i.getItemSelectable();
+                Object select[] = is.getSelectedObjects();
+                this.method = (String)select[0];
+                System.out.println(method);
+            }
+        }
     }
 }
 
--- smoothing.java
+++ smoothing.java
@@ -11,103 +11,120 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
+import java.awt.image.RasterFormatException;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
-public class smoothing extends JFrame {
-
-	JPanel contentPane;
-	Rectangle captureRect;
-
-	/**
-	 * Create the frame.
-	 */
-	public smoothing(String imgPath) {
-		setBounds(100, 100, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		final JLabel lblPicture = new JLabel("");
-		lblPicture.setOpaque(true);
-//		lblPicture.setBounds(6, 6, 661, 476);
-		
-		
-		BufferedImage img = null;
-		try {
-		    img = ImageIO.read(new File(imgPath));
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
-//		Image dimg = img.getScaledInstance(lblPicture.getWidth(), lblPicture.getHeight(), Image.SCALE_SMOOTH);
-//		ImageIcon imageIcon = new ImageIcon(dimg);
-//		lblPicture.setIcon(imageIcon);
-		
-		final BufferedImage final_img = img;   // have to make a final version 
-		
-		final BufferedImage screenCopy = new BufferedImage( // a copy version of the image
-				final_img.getWidth(),
-				final_img.getHeight(),
-				final_img.getType());
-		
-		
-		lblPicture.setIcon(new ImageIcon(screenCopy));
-		
-		JScrollPane screenScroll = new JScrollPane(lblPicture);   // Scroll Pane stores the image label
-		screenScroll.setBounds(6,6,661,476);
-		
-		contentPane.add(screenScroll);
-		
-		JLabel lblSelection = new JLabel("Drag a rectangle to be smoothed");
-		lblSelection.setBounds(29, 498, 592, 50);
-		contentPane.add(lblSelection);
-		
-
-		
-		repaint(final_img,screenCopy);
-		lblPicture.repaint();
-		
-		lblPicture.addMouseMotionListener(new MouseMotionAdapter() {
-			Point start = new Point();
-			
-			@Override
+public class smoothing extends JFrame implements ActionListener{
+    
+    JPanel contentPane;
+    Rectangle captureRect;
+    JButton btnProcess;
+    final BufferedImage final_img;
+    final JLabel lblPicture;
+    
+    /**
+     * Create the frame.
+     */
+    public smoothing(String imgPath) {
+        setBounds(100, 100, 800, 600);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        lblPicture = new JLabel("");
+        lblPicture.setOpaque(true);
+        //		lblPicture.setBounds(6, 6, 661, 476);
+        
+        
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(imgPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //		Image dimg = img.getScaledInstance(lblPicture.getWidth(), lblPicture.getHeight(), Image.SCALE_SMOOTH);
+        //		ImageIcon imageIcon = new ImageIcon(dimg);
+        //		lblPicture.setIcon(imageIcon);
+        
+        final_img = img;   // have to make a final version 
+        
+        final BufferedImage screenCopy = new BufferedImage( // a copy version of the image
+                                                           final_img.getWidth(),
+                                                           final_img.getHeight(),
+                                                           final_img.getType());
+        
+        
+        lblPicture.setIcon(new ImageIcon(screenCopy));
+        
+        JScrollPane screenScroll = new JScrollPane(lblPicture);   // Scroll Pane stores the image label
+        screenScroll.setBounds(6,6,661,476);
+        
+        contentPane.add(screenScroll);
+        
+        JLabel lblSelection = new JLabel("Drag a rectangle to be smoothed"); // information label
+        lblSelection.setBounds(29, 498, 592, 50);
+        contentPane.add(lblSelection);
+        
+        btnProcess = new JButton("Process");  // process button
+        btnProcess.addActionListener(this);
+        btnProcess.setBounds(654, 526, 117, 29);
+        contentPane.add(btnProcess);
+        
+        
+        
+        
+        repaint(final_img,screenCopy);
+        lblPicture.repaint();
+        
+        lblPicture.addMouseMotionListener(new MouseMotionAdapter() {
+            Point start = new Point();
+            
+            @Override
             public void mouseMoved(MouseEvent me) {
-				start = me.getPoint();
-				repaint(final_img,screenCopy);
-				lblSelection.setText("Start Point: " + start);
-				
-				
-				lblPicture.repaint();
-			}
-			
-			@Override
+                start = me.getPoint();
+                repaint(final_img,screenCopy);
+                lblSelection.setText("Start Point: " + start);
+                
+                
+                lblPicture.repaint();
+            }
+            
+            @Override
             public void mouseDragged(MouseEvent me) {
-				Point end = me.getPoint();
-				captureRect = new Rectangle(start,
-                        new Dimension(end.x-start.x, end.y-start.y));
-				repaint(final_img, screenCopy);
-				lblPicture.repaint();
-				lblSelection.setText("Rectangle: " + captureRect);
-			}
-			
-		});
-		
-		
-		
-	}
-	
-	public void repaint(BufferedImage orig, BufferedImage copy){
-		Graphics2D g = copy.createGraphics();
+                Point end = me.getPoint();
+                captureRect = new Rectangle(start,
+                                            new Dimension(end.x-start.x, end.y-start.y));
+                repaint(final_img, screenCopy);
+                lblPicture.repaint();
+                lblSelection.setText("Rectangle: " + captureRect);
+            }
+            
+        });
+        
+        
+        
+    }
+    
+    
+    
+    public void repaint(BufferedImage orig, BufferedImage copy){
+        Graphics2D g = copy.createGraphics();
         g.drawImage(orig,0,0, null);
         if (captureRect!=null) {
             g.setColor(Color.RED);
@@ -116,6 +133,26 @@ public class smoothing extends JFrame {
             g.fill(captureRect);
         }
         g.dispose();
-	}
-
+    }
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        BufferedImage smoothingPart;
+        
+        if(e.getSource() == btnProcess){
+            try{
+                smoothingPart = final_img.getSubimage(captureRect.x, captureRect.y, captureRect.width, captureRect.height);
+                lblPicture.setIcon(new ImageIcon(smoothingPart));
+            }catch(RasterFormatException d){
+                JOptionPane.showMessageDialog(null, "please select an area first");
+            }
+            
+            
+        }
+    }
+    
+    
+    
 }
