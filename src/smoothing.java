74d866697a5c912ba32636d1a6be83c0a9a42822
Haoran Fang <gougou9903@gmail.com>
2015-04-25 18:52:00 -0600

    Parallel in both Blurring and Enlarging
    This improves speed a lot.  Some minor errors exists

--- src/smoothing.java
+++ src/smoothing.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
@@ -145,34 +146,77 @@ public class smoothing extends JFrame implements ActionListener{
 		BufferedImage smoothingPartSource;
 		BufferedImage smoothingPartTarget;
 		BufferedImage smoothingTarget = final_img;
+		BufferedImage innerEnlargeTarget;
+		BufferedImage outerEnlargeTarget;
 		BufferedImage enlargeTarget;
 		
-		if(e.getSource() == btnProcess){
+		if(e.getSource() == btnProcess){    // process button
 			long startTime = System.nanoTime();
 			try{
-				//long start = System.currentTimeMillis();
+				/*dig out the smoothing part, enlarging the rest using 4 threads*/
+				BufferedImage[] subImage_out = new BufferedImage[4];        //divide this ring into 4 parts
+		        BufferedImage[] subOutImage_out = new BufferedImage[4];     //store enlarged image
+		        Thread[] subThreads = new Thread[4];
+		        
+		        int imgWidth = final_img.getWidth();
+		        int imgHeight = final_img.getHeight();
+		        subImage_out[0] = final_img.getSubimage(0, 0, captureRect.x, imgHeight);
+		        subImage_out[1] = final_img.getSubimage(captureRect.x, 0, captureRect.width, captureRect.y);
+		        subImage_out[2] = final_img.getSubimage(captureRect.x+captureRect.width, 0, imgWidth-captureRect.x-captureRect.width, imgHeight);
+		        subImage_out[3] = final_img.getSubimage(captureRect.x,captureRect.y+captureRect.height,captureRect.width,imgHeight-captureRect.y-captureRect.height);
 				
-				/*Gaussian Blur*/
+		        for(int i=0; i<4 ;i++){
+		        	subOutImage_out[i] = new BufferedImage((int)(subImage_out[i].getWidth() * ratio)-1, (int)(subImage_out[i].getHeight() * ratio)-1,BufferedImage.TYPE_INT_RGB );
+                    subThreads[i] = new Thread(new EnlargeParallel(subImage_out[i], subOutImage_out[i], ratio));
+                    subThreads[i].start();
+		        }
+		        
+		        /*Gaussian Blur*/
 				smoothingPartSource = final_img.getSubimage(captureRect.x, captureRect.y, captureRect.width, captureRect.height);
 				GaussianBlur GB = new GaussianBlur();
-				smoothingPartTarget = GB.gaussianBlur(smoothingPartSource, 1.4);
+				smoothingPartTarget = GB.gaussianBlur(smoothingPartSource, 0);
 				
-				int w = smoothingPartTarget.getWidth();
-				int h = smoothingPartTarget.getHeight();
+				/*Enlarge Blurring part*/
+				Enlarge E = new Enlarge(smoothingPartTarget,ratio);
+				innerEnlargeTarget = E.enlarge();
+		        
+		        for (int i = 0; i < 4; i++) {  
+		            try {
+		                subThreads[i].join();
+		            } catch (Exception ex) {
+		            }
+		        }
+		        
+		        
+		        /*repaint 4 parts*/
+		        outerEnlargeTarget = new BufferedImage((int)(imgWidth*ratio)-1, (int)(imgHeight*ratio)-1,BufferedImage.TYPE_INT_RGB);
+		        Graphics g = outerEnlargeTarget.getGraphics();
+		        g.drawImage(subOutImage_out[0], 0, 0, null);
+		        g.drawImage(subOutImage_out[1], subOutImage_out[0].getWidth(), 0, null);
+		        g.drawImage(subOutImage_out[2], subOutImage_out[0].getWidth()+subOutImage_out[1].getWidth(), 0, null);
+		        g.drawImage(subOutImage_out[3], subOutImage_out[0].getWidth(), outerEnlargeTarget.getHeight()-subOutImage_out[3].getHeight(), null);
+		        g.drawImage(innerEnlargeTarget, subOutImage_out[0].getWidth(),subOutImage_out[1].getHeight()+1,null);
+		        g.dispose();
+		        
+		        
 				
-				/*put partTarget back to Target*/
-				for(int i=0 ; i < w; i++){
-					for(int j=0 ; j < h; j++){
-						smoothingTarget.setRGB(captureRect.x + i, captureRect.y + j, smoothingPartTarget.getRGB(i, j));
-					}
-				}
+				
+//				int w = smoothingPartTarget.getWidth();
+//				int h = smoothingPartTarget.getHeight();
+				
+//				/*put partTarget back to Target*/
+//				for(int i=0 ; i < w; i++){
+//					for(int j=0 ; j < h; j++){
+//						smoothingTarget.setRGB(captureRect.x + i, captureRect.y + j, smoothingPartTarget.getRGB(i, j));
+//					}
+//				}
 				
 				/*enlarge this smoothed image*/
 				
-				Enlarge E = new Enlarge(smoothingTarget,ratio);
-				enlargeTarget = E.enlarge();
+//				Enlarge E = new Enlarge(smoothingTarget,ratio);
+//				enlargeTarget = E.enlarge();
 				
-			lblPicture.setIcon(new ImageIcon(enlargeTarget));
+			lblPicture.setIcon(new ImageIcon(outerEnlargeTarget));
 			
 			
 			}catch(RasterFormatException d){
